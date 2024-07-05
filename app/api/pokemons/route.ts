3631f824802cea09f8304a56105f9242ffcfa814
raccoonboy <raccoonboy0803@gmail.com>
2024-07-05 16:41:33 +0900

    Feat: 포켓몬리스트 무한스크롤 구현

--- app/api/pokemons/route.ts
+++ app/api/pokemons/route.ts
@@ -2,24 +2,31 @@ import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 const TOTAL_POKEMON = 151;
+const PAGE_SIZE = 20;
+
+const isNotNull = <T>(value: T | null): value is T => value !== null;
 
 export const GET = async (request: Request) => {
+  const { searchParams } = new URL(request.url);
+  const page = parseInt(searchParams.get('page') || '1');
+  const offset = (page - 1) * PAGE_SIZE;
+
   try {
-    const allPokemonPromises = Array.from(
-      { length: TOTAL_POKEMON },
-      (_, index) => {
-        const id = index + 1;
-        return Promise.all([
-          axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
-          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
-        ]);
-      }
-    );
+    const allPokemonPromises = Array.from({ length: PAGE_SIZE }, (_, index) => {
+      const id = index + 1 + offset;
+      if (id > TOTAL_POKEMON) return null;
+
+      return Promise.all([
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
+        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+      ]);
+    }).filter(isNotNull);
 
     const allPokemonResponses = await Promise.all(allPokemonPromises);
+    if (allPokemonResponses === null) return;
 
     const allPokemonData = allPokemonResponses.map(
-      ([response, speciesResponse], index) => {
+      ([response, speciesResponse]) => {
         const koreanName = speciesResponse.data.names.find(
           (name: any) => name.language.name === 'ko'
         );
--- components/pokemonList.tsx
+++ components/pokemonList.tsx
@@ -1,57 +1,82 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React, { useRef } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Image from 'next/image';
 import { Pokemon } from '../types/apiType';
 import { useRouter } from 'next/navigation';
+import { useInView } from 'react-intersection-observer';
 
-const fetchPokemons = async () => {
-  try {
-    const response = await axios.get('/api/pokemons');
-    return response.data;
-  } catch (error) {}
+const fetchPokemons = async ({ pageParam = 1 }) => {
+  const response = await axios.get(`/api/pokemons?page=${pageParam}`);
+  return response.data;
 };
 
 export default function PokemonList() {
   const router = useRouter();
+  const observerRef = useRef<IntersectionObserver | null>(null);
 
-  const { data, isFetched } = useQuery<Pokemon[]>({
-    queryKey: ['getPokemon'],
+  const {
+    data: pokemons,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isError,
+    isPending,
+  } = useInfiniteQuery({
+    queryKey: ['pokemonsInfinite'],
     queryFn: fetchPokemons,
+    getNextPageParam: (lastPage, _, lastPageParam) => {
+      const nextPage = lastPageParam + 1;
+      return lastPage.length === 20 ? nextPage : undefined;
+    },
+    select: ({ pages }) => pages.flat(),
+    initialPageParam: 1,
   });
 
-  if (!isFetched) {
-    return <div>Loading...</div>;
-  }
+  const observeElement = (el: HTMLDivElement) => {
+    if (!el || observerRef.current || !hasNextPage || isFetchingNextPage)
+      return;
+
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        fetchNextPage();
+      }
+    });
+
+    observerRef.current.observe(el);
+  };
+
+  if (isPending) return <div>Loading...</div>;
+
+  if (isError) return <div>fetching Error</div>;
 
   return (
     <div className="w-[80%] flex flex-col justify-center items-center mx-auto py-5">
       <div className="grid grid-cols-6 gap-4 w-full mx-4 justify-items-center">
-        {isFetched &&
-          data?.map((pokemon) => (
-            <div
-              onClick={() => {
-                router.push(`/pokemons/${pokemon.id}`);
-              }}
-              className="w-full flex flex-col items-center border rounded-md border-gray-300 my-2 mx-2 cursor-pointer hover:bg-purple-500 "
-              key={pokemon.id}
-            >
-              <Image
-                src={pokemon.sprites.front_default}
-                alt={pokemon.name}
-                width={100}
-                height={100}
-                priority
-              />
-              <h3 className="text-yellow-500 text-2xl mt-2">
-                {pokemon.korean_name}
-              </h3>
-              <p className="text-white text-lg mb-2">도감번호 : {pokemon.id}</p>
-            </div>
-          ))}
+        {pokemons.map((pokemon: Pokemon) => (
+          <div
+            onClick={() => {
+              router.push(`/pokemons/${pokemon.id}`);
+            }}
+            className="w-full flex flex-col items-center border rounded-md border-gray-300 my-2 mx-2 cursor-pointer hover:bg-red-700  hover:border-yellow-500"
+            key={pokemon.id}
+          >
+            <Image
+              src={pokemon.sprites.front_default}
+              alt={pokemon.name}
+              width={100}
+              height={100}
+            />
+            <h3 className="text-yellow-500 text-2xl mt-2">
+              {pokemon.korean_name}
+            </h3>
+            <p className="text-white text-lg mb-2">도감번호 : {pokemon.id}</p>
+          </div>
+        ))}
       </div>
+      <div className="h-20" ref={observeElement}></div>
     </div>
   );
 }
